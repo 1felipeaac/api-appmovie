3cd0da6c16910d8103e3590a9dad34abf5d2458c
Felipe Coelho <felipeaacoelho@gmail.com>
2023-09-15 14:03:57 -0300

    teste de email existente

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,5 +1,7 @@
 const UserCreateService = require("./UserCreateService")
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const {hash, compare} = require("bcryptjs")
+const AppError = require("../utils/AppError");
 
 describe("UserCreateService", () =>{
     it("user should be create", async ()=>{
@@ -13,6 +15,26 @@ describe("UserCreateService", () =>{
         const userCreateService = new UserCreateService(userRepositoryInMemory)
         const userCreated = await userCreateService.execute(user)
             expect(userCreated).toHaveProperty("id")
+    })
+
+    it("user not should be created with email already exists", async ()=>{
+        const user1 = {
+            name: "User test 1",
+            email: "user@test.com",
+            password: "123"
+        }
+        const user2 = {
+            name: "User test 2",
+            email: "user@test.com",
+            password: "231"
+        }
+
+        const userRepository = new UserRepositoryInMemory()
+        const userCreateService = new UserCreateService(userRepository)
+
+        await userCreateService.execute(user1)
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso"))
+
     })
 
 })
